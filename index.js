0b68a223999549dea105be5a530a1be5bc082619
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-28 16:10:50 +0000

    step 2: refactor functions

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ function newTask(title, description){
   return task;
 }
 
+// print task details
+function logTaskState(task){
+  console.log(`${task.title} has ${task.complete ? '': 'not '}been completed.`);
+}
+
+// mark it complete
+function completeTask(task){
+  task.complete = true;
+}
+
 const task1 = newTask('Clean the cat box.', 'Note: see it the cat is happier now...')
 const task2 = newTask('Do laundry.', 'Note: do not forget it in the dryer again...')
 const tasks = [task1, task2]
+
 // DRIVER CODE BELOW
-console.log(tasks)
+logTaskState(task1);
+completeTask(task1);
+logTaskState(task1);
+
+console.log(tasks);
