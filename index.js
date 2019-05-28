ec538541df12c14ffd3ac637bea372023cc3fc32
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-28 16:19:37 +0000

    step 3: include functions as methods

--- index.js
+++ index.js
@@ -3,28 +3,29 @@ function newTask(title, description){
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    // include functions as methods
+    logState: function(){
+      console.log(`${this.title} has ${this.complete ? '': 'not '}been completed.`);
+    },
+    markCompleted: function(){
+      this.complete = true;
+    }
   };
   return task;
 }
 
-// print task details
-function logTaskState(task){
-  console.log(`${task.title} has ${task.complete ? '': 'not '}been completed.`);
-}
 
-// mark it complete
-function completeTask(task){
-  task.complete = true;
-}
+
+
 
 const task1 = newTask('Clean the cat box.', 'Note: see it the cat is happier now...')
 const task2 = newTask('Do laundry.', 'Note: do not forget it in the dryer again...')
 const tasks = [task1, task2]
 
 // DRIVER CODE BELOW
-logTaskState(task1);
-completeTask(task1);
-logTaskState(task1);
+task1.logState();
+task1.markCompleted();
+task1.logState();
 
-console.log(tasks);
+
